15093ec26f1cbf3c023941aef38539ed33ad2d4d
mom0tomo <momo76w@gmail.com>
2018-11-10 17:52:21 -0600

    Added functionality to create user's own question and answer

---
+++ 03_lab_3_readme.md
@@ -0,0 +1,20 @@
+📝 `NOTE` Paste this template at the top of your existing `README.md` file from the last lab.
+
+## Lab 3
+
+### App Description
+User can create a card and still see their created card when the app is relaunched.
+User can create multiple cards and browse through all created cards.
+
+### App Walk-though
+<img src="https://i.imgur.com/9Y84dIb.gif" width=200><br>
+
+## Required
+- [x] User can create a card and still see their created card when the app is relaunched.
+- [x] User can create multiple cards and browse through all created cards
+
+## Optional
+- [] User can delete a card and no longer see it in their deck anymore
+- [ ] Flashcards are shown in random order instead of the order they were created in
+- [ ] User can edit a card and see the edit saved when they browse through their deck of cards
+- [ ] User can create flashcards with multiple answers and be able to browse through cards with multiple choice answers shown
--- app/src/main/java/com/example/kaerah/flashcardapplication/MainActivity.java
+++ app/src/main/java/com/example/kaerah/flashcardapplication/MainActivity.java
@@ -6,22 +6,79 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if(currentCardDisplayedIndex == 0)
+        {
+            ((TextView) findViewById(R.id.flashcard_question)).setText("Add a Card!");
+        }
+
         findViewById(R.id.addBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                intent.putExtra("stringKey1", "harry potter");
-                intent.putExtra("stringKey2", "voldemort");
-                MainActivity.this.startActivityForResult(intent, 100);
+            Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+            MainActivity.this.startActivityForResult(intent, 100);
 
             }
         });
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+
+        findViewById(R.id.nextBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+            }
+        });
+
+        findViewById(R.id.deleteBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView)findViewById(R.id.flashcard_question)).getText().toString());
+                currentCardDisplayedIndex--;
+                if(currentCardDisplayedIndex == 0)
+                {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("Add a Card!");
+                }
+                allFlashcards = flashcardDatabase.getAllCards();
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+
+        } );
+
+
     }
 
     public void onClick1(View v) {
@@ -36,13 +93,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
-            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
-            String string2 = data.getExtras().getString("string2");
-            ((TextView)findViewById(R.id.flashcard_question)).setText(string1);
-            ((TextView)findViewById(R.id.flashcard_answer)).setText(string2);
+            String question = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String answer = data.getExtras().getString("string2");
+            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
-    }
 
+    }
 
 }
 
